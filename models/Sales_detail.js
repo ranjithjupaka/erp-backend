8ac264a40fe7a8a3ba82b2397f59de29b7562fa5
Prathamesh28 <53966460+Prathamesh28@users.noreply.github.com>
2021-11-23 12:57:28 +0530

    Update Sales_detail.js

--- models/Sales_detail.js
+++ models/Sales_detail.js
@@ -4,23 +4,18 @@ const SalesSchema = new mongoose.Schema(
   {
     selling_type: {
       type: String,
-      required: true,
     },
     selling_price: {
       type: Number,
-      required: true,
     },
     selling_discount: {
       type: Number,
-      required: true,
     },
     selling_gst: {
       type: Number,
-      required: true,
     },
     selling_total: {
       type: Number,
-      required: true,
     },
     status:{
       type:String
