9c49b263c9db309630a7ef1cf8f4109916c10d19
Prathamesh28 <53966460+Prathamesh28@users.noreply.github.com>
2021-11-23 13:02:08 +0530

    Update Purchase_detail.js

--- models/Purchase_detail.js
+++ models/Purchase_detail.js
@@ -3,18 +3,23 @@ const PurchaseSchema = new mongoose.Schema(
   {
     quote: {
       type: String,
+      default : "",
     },
     purchase_type: {
       type: String,
+      default : "",
     },
     purchase_price: {
       type: Number,
+      default: 0,
     },
     discount: {
       type: Number,
+      default : 0,
     },
     gst: {
       type: Number,
+      default : 0,
     },
     total: Number,
     availability: {
